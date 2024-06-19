be59993f89181c63cbe323b31873d25e2b4a4520
diogohc <diogo.h.correia@gmail.com>
2024-06-19 12:15:57 +0100

    add logging and hide sensitive keys

--- demo/src/main/java/MyImdb/demo/enums/AddExternalMovieStatus.java
+++
@@ -1,9 +0,0 @@
-package MyImdb.demo.enums;
-
-public enum AddExternalMovieStatus {
-    MOVIE_ALREADY_EXISTS_IN_DB,
-    MOVIE_NOT_SAVED,
-    MOVIE_SAVED_SUCCESSFULLY,
-    INCORRECT_IMDB_ID,
-    ONLY_ACCEPT_MOVIES
-}
